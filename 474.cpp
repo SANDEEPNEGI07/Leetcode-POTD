610ccf9d9b6d61db972cfaef812d2c7dfa49be3e
Sandeep Singh <sandeepnegi5898@gmail.com>
2025-11-11 15:19:18 +0530

    11 November 2025

---
+++ 474.cpp
@@ -0,0 +1,25 @@
+// 474. Ones and Zeroes
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+public:
+    int findMaxForm(vector<string> &strs, int m, int n){
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+
+        for(auto &s : strs){
+            int zeroes = count(s.begin(), s.end(), '0');
+            int ones = count(s.begin(), s.end(), '1');
+
+            for(int i = m; i >= zeroes; i--){
+                for(int j = n; j >= ones; j--){
+                    dp[i][j] = (dp[i][j], dp[i - zeroes][j - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
